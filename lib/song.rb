1a106c91d61fd4d7e76fffb588a54aa8b8812071
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 06:58:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,14 @@
 
 class Song
   attr_accessor :artist , :name
+  @@all_songs = []
+
   def initialize (name )
     self.name = name
   end
   def artist_name
     self.artist.name if artist
   end
+  def self.song_count
+    
 end
