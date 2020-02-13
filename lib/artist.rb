61841b92a1f6f28cfbfd4f86fd9c170442fa3a9c
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 06:58:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,10 @@ class Artist
 
   attr_accessor :name ,:songs
   @@all_songs = []
+  def initialize(name)
+    @name = name 
+    songs = [] 
+  end
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     song.artist=self
