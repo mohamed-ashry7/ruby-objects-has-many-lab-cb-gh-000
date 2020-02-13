8b66f46ef0861ae3eddedd7bebf033ce402215f9
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 07:00:07 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,13 +2,13 @@
 
 class Post
   attr_accessor :author , :title
-  @@all_songs = []
-  def initialize (name )
-    self.name = name
-    @@all_songs << self
+  @@all_posts = []
+  def initialize (title )
+    self.title = title
+    @@all_posts << self
   end
   def artist_name
-    self.artist.name if artist
+    self.author.name if author
   end
   def self.song_count
     @@all_songs
